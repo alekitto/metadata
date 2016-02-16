6fb60b4d89784e5659db6fba3cf8d0175c546262
Alessandro Chitolina <alekitto@gmail.com>
2016-02-17 00:33:46 +0100

    added chain loader

---
+++ lib/Loader/ChainLoader.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Kcs\Metadata\Loader;
+
+use Kcs\Metadata\ClassMetadataInterface;
+
+class ChainLoader implements LoaderInterface
+{
+    /**
+     * @var LoaderInterface[]
+     */
+    private $loaders;
+
+    /**
+     * ChainLoader constructor
+     *
+     * @param LoaderInterface[] $loaders
+     */
+    public function __construct(array $loaders)
+    {
+        foreach ($loaders as $loader) {
+            if (! $loader instanceof LoaderInterface) {
+                throw new \InvalidArgumentException(sprintf("Class %s is expected to implement LoaderInterface", get_class($loader)));
+            }
+        }
+
+        $this->loaders = $loaders;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function loadClassMetadata(ClassMetadataInterface $classMetadata)
+    {
+        $success = false;
+
+        foreach ($this->loaders as $loader) {
+            $success = $loader->loadClassMetadata($classMetadata) || $success;
+        }
+
+        return $success;
+    }
+}
---
+++ tests/Loader/ChainLoaderTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Kcs\Metadata\Tests\Loader;
+
+use Kcs\Metadata\ClassMetadata;
+use Kcs\Metadata\Loader\ChainLoader;
+use Prophecy\Argument;
+
+class ChainLoaderTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     * @expectedException \InvalidArgumentException
+     */
+    public function constructor_should_throw_on_non_loader_instance()
+    {
+        $loaders = [
+            new \stdClass()
+        ];
+
+        new ChainLoader($loaders);
+    }
+
+    /**
+     * @test
+     */
+    public function load_metadata_should_call_all_loaders()
+    {
+        $loader1 = $this->prophesize('Kcs\Metadata\Loader\LoaderInterface');
+        $loader2 = $this->prophesize('Kcs\Metadata\Loader\LoaderInterface');
+        $loader3 = $this->prophesize('Kcs\Metadata\Loader\LoaderInterface');
+
+        $metadata = new ClassMetadata(new \ReflectionClass($this));
+
+        $loader1->loadClassMetadata($metadata)->shouldBeCalled();
+        $loader2->loadClassMetadata($metadata)->shouldBeCalled();
+        $loader3->loadClassMetadata($metadata)->shouldBeCalled();
+
+        $loader = new ChainLoader([
+            $loader1->reveal(),
+            $loader2->reveal(),
+            $loader3->reveal()
+        ]);
+        $loader->loadClassMetadata($metadata);
+    }
+}
